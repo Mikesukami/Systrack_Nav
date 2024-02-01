952dfbe734171afc1a8a71695b73be1b5e747f4d
Mikesukami <mikesu2544@gmail.com>
2024-02-01 10:10:41 +0700

    Update build again

--- systrack/src/app.config.js
+++ systrack/src/app.config.js
@@ -1,4 +1,4 @@
-// export const SERVER_URL = "http://localhost:4080/api/";
-export const SERVER_URL = "https://systrack.onrender.com/api/";
+export const SERVER_URL = "http://localhost:4080/api/";
+// export const SERVER_URL = "https://systrack.onrender.com/api/";
 
 // export const SERVER_URL =  "https://systrack.netlify.app/api/";
